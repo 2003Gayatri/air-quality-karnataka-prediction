ac44ebf70c417f301ad44b31a831651e88a75958
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2025-06-25 14:03:16 +0530

    Setup to automate the requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    The following function will return the list of requirements
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e.':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.text File not found")
+
+    return requirement_lst
+
+setup(
+    name ="Air Quality Karnataka Prediction",
+    version="0.0.1",
+    author="Srithan,Gayatri,Rajat",
+    packages=find_packages(),
+    install_requires =get_requirements()
+)
